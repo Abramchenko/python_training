d142c37d78b20b5b7642a5d79a87dd31809d2102
ANV <anv-anv@mail.ru>
2023-02-23 18:28:15 +0600

    Задание 4. Перевод на pytest.

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,88 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+import pytest
 from contact import Contact
-import unittest
+from application import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinilizer(fixture.destroy)
+    return fixture
+def test_add_contact(app):
 
-    
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_addnew_page(wd)
-        self.create_contact(wd, Contact(firsrtname="Alexei", lastname="Abramchenko", address="Omsk", company="IT one",
-                                        nickname="Alex", home="8(3812)463325", mobile="89139792905",
-                                        email="anv-anv@mail.ru", bday="28", bmonth="May", byear="2019"))
-        self.logout(wd)
-    '''
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_addnew_page(wd)
-        self.create_contact(wd, Contact(firsrtname="", lastname="", address="", company="",
-                                        nickname="", home="", mobile="",email="", bday="", bmonth="", byear=""))
-        self.logout(wd)
-    '''
-    def create_contact(self, wd, contact):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firsrtname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-       # x=contact.bday
-       # wd.find_element_by_xpath("//option[@value="+x+"]").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        #wd.find_element_by_xpath("//option[@value="+contact.bmonth+"]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        #enter_contact(self, wd):
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def open_addnew_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def open_home_page(self, wd):
-        wd.get("https://localhost/addressbook/")
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-    def tearDown(self):
-        self.wd.quit()
+    app.login( username="admin", password="secret")
+    app.create_contact( Contact(firsrtname="Alexei", lastname="Abramchenko", address="Omsk", company="IT one",
+                                nickname="Alex", home="8(3812)463325", mobile="89139792905",
+                                email="anv-anv@mail.ru", bday="28", bmonth="May", byear="2019"))
+    app.logout()
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+
+
+
