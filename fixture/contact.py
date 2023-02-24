fb82ff1fb100680701bd8c9e84a03dc20f22f82f
ANV <anv-anv@mail.ru>
2023-02-24 15:57:18 +0600

    Задание 5. Реализована двухуровневая архитектура тестов: созданы классы-помощники и класс-менеджер.

---
+++ fixture/contact.py
@@ -0,0 +1,47 @@
+class ContactHelper():
+    def __init__(self, app):
+        self.app = app
+
+    def open_addnew_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_addnew_page()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firsrtname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        # enter_contact(self, wd):
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,17 +3,18 @@ import pytest
 from model.contact import Contact
 from fixture.application import Application
 
+
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-def test_add_contact(app):
 
+
+def test_add_contact(app):
     app.session.login( username="admin", password="secret")
-    app.create_contact( Contact(firsrtname="Alexei", lastname="Abramchenko", address="Omsk", company="IT one",
-                                nickname="Alex", home="8(3812)463325", mobile="89139792905",
-                                email="anv-anv@mail.ru", bday="28", bmonth="May", byear="2019"))
+    app.contact.create(Contact(firsrtname="Alexei", lastname="Abramchenko", address="Omsk", company="IT one",nickname="Alex", home="8(3812)463325",
+                               mobile="89139792905", email="anv-anv@mail.ru", bday="28", bmonth="May", byear="2019"))
     app.session.logout()
 
 
