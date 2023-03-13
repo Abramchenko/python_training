292bce4620a94577876ecedfdc0db1b86bcba7c5
ANV <anv-anv@mail.ru>
2023-03-13 21:51:19 +0600

    Улучшена функция чтения username.

--- fixture/session.py
+++ fixture/session.py
@@ -11,6 +11,7 @@ class SessionHelper():
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # или wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
@@ -38,5 +39,9 @@ class SessionHelper():
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("/html/body/div/div[1]/form/b").text == "("+username+")"
+        return self.get_logged_user()== username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("/html/body/div/div[1]/form/b").text[1:-1]
 
