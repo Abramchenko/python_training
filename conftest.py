0c4a2efbcf23faf2a22696d4e02aef47a568a827
ANV <anv-anv@mail.ru>
2023-03-21 20:17:41 +0600

    17. Загрузка параметров из конфиг файла.

--- conftest.py
+++ conftest.py
@@ -1,20 +1,26 @@
-import pytest
 from fixture.application import Application
-#perem =None
+import pytest
+import json
+import os.path
+
+
 fixture = None
+target = None
 @pytest.fixture
 #(scope="session") фикстура в процессе выполнения тестов может закрыться (внепланово), поэтому лучше создавать ее не однажды на всю сессию,
 # а проверять ее валидность перед тестом, и создавать по необходимости
 def app(request):
-    global fixture
+    global fixture # говорим, что будем их использовать
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--base_url")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid(): # если что, перезапустить фикстуру
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")   # гарантированный логин
+    if target is None:   # чтобы открыть его и считать однажды вначале
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+
+    if fixture is None or not fixture.is_valid(): # если что, перезапустить фикстуру
+        fixture = Application(browser=browser, base_url= target["base_url"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])   # гарантированный логин
     return fixture
 
 # такая фикстура выполняется один раз, в конце
@@ -29,4 +35,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
+#"http://localhost/addressbook/")
