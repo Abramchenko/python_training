d254bb4057764c98379e2859fc675004a9c282a7
ANV <anv-anv@mail.ru>
2023-04-10 22:03:48 +0600

    22.1. Добавление контактов в группы. Удаление  контакта из группы.

--- test/test_add_somecontact_to_group.py
+++ test/test_add_somecontact_to_group.py
@@ -1,8 +1,7 @@
 from model.contact import Contact
-from model.group import Group
 import random
 
-def test_add_contact_to_group(app, db, orm, json_contacts, check_ui):
+def test_add_contact_to_group(app, orm):
     if len(orm.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Ivan", lastname="Petrovich", nickname="Alex", title=" some title",
                                company="IT one"))
@@ -16,5 +15,3 @@ def test_add_contact_to_group(app, db, orm, json_contacts, check_ui):
     new_contacts_in_group = orm.get_contacts_in_group(group)
     # сравнение списков по содержанию, добавим такой же элемент в старый список
     assert sorted(old_contacts_in_group, key = Contact.id_or_max) == sorted(new_contacts_in_group, key = Contact.id_or_max)
-    #if check_ui:
-    #    assert sorted(new_contacts, key=Contact.id_or_max)==sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -1,6 +1,7 @@
 import random
+from model.contact import Contact
 
-def test_del_contact_from_group(app, db, orm, check_ui):
+def test_del_contact_from_group(app, orm):
     groups = orm.get_group_list()
     group = random.choice(groups)
     old_contacts_in_group = orm.get_contacts_in_group(group)
@@ -9,11 +10,8 @@ def test_del_contact_from_group(app, db, orm, check_ui):
         contact = random.choice(not_in_group)
         app.contact.add_contact_to_group(contact.id, group.name)
         old_contacts_in_group = orm.get_contacts_in_group(group)
-
     contact = random.choice(old_contacts_in_group)
-
-    #print('\n', contact.id, group.id, group.name)
-    app.contact.remove_contact_from_group(contact.id, group)
+    app.contact.remove_contact_from_group(contact, group)
     old_contacts_in_group.remove(contact)
     new_contacts_in_group = orm.get_contacts_in_group(group)
-    assert old_contacts_in_group == new_contacts_in_group
+    assert sorted(new_contacts_in_group, key=Contact.id_or_max) == sorted(old_contacts_in_group, key=Contact.id_or_max)
